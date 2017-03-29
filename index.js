d65ca65f70516f40b6454e137b0135b7251c0c25
Andrea Baty <Andrea.f.baty@gmail.com>
2017-03-29 18:08:53 -0400

    updating proxy

--- index.js
+++ index.js
@@ -12,36 +12,77 @@ app.get('/test', function(req, res){ // listens for request on /api route
 
 /* PUT YOUR CODE BETWEEN COMMENTS */
 
-app.get('/v3/oauth/request', function(req, res){
-  var consumerKey = req.query.consumer_key;
-  var redirectUri = req.query.redirect_uri;
+var config = {
+ url: {
+   request: "https://getpocket.com/v3/oauth/request",
+   authorize: "https://getpocket.com/v3/oauth/authorize",
+   gett: "https://getpocket.com/v3/get"
+ },
+ headers: {
+   "content-type": "application/x-www-form-urlencoded",
+   "X-Accept": "application/json"
+ }
+}
 
-  var url = 'https://getpocket.com/v3/oauth/request?' +
-            'consumer_key=' + consumerKey + '&' +
-            'key=64510-02865d3dfc874b4fa05491f8'
+// STEP 1: OBTAIN A PLATFORM CONSUMER KEY; TYPE STRING
 
-  request(url, function(error, response, body){
-    res.send(body);
-  });
-})
+const CONSUMER_KEY = '64685-8fb9b370179438511c36a982';
 
+// var request_token;
+
+// STEP 2: OBTAIN A REQUEST TOKEN
+
+app.get('/request', function(req, res) {
+ var options = {
+   headers: config.headers,
+   url: config.url.request,
+   body: "consumer_key=" + CONSUMER_KEY + "&redirect_uri=" + config.url.redirect,
+ };
+
+ request.post(options, function(error, response, body) {
+   var request_token = JSON.parse(body).code;
+   var url = "https://getpocket.com/auth/authorize" + '?request_token=' + request_token + '&redirect_uri=' + 'http://127.0.0.1:8080/dist/#habitdetail/' + req.query.habitId;
+
+   res.send({
+     url,
+     request_token
+   });
+ });
+});
+
+// STEP 3
+
+// STEP 4: CONVERT YOUR REQUEST TOKEN INTO A POCKET ACCESS TOKEN
+
+app.get('/token', function(req, res) {
+ var options = {
+   headers: config.headers,
+   url: config.url.authorize,
+   body: "consumer_key=" + CONSUMER_KEY + "&code=" + req.query.request_token + '&redirect_uri=' + config.url.redirect
+ }
+
+ request.post(options, function(error,response, body){
+   var access_token = JSON.parse(body).access_token;
+   res.send(access_token)
+ });
+});
+
+// STEP 6: USE POCKET ACCESS TOKEN TO RETRIEVE FROM POCKET
+
+app.get('/gett', function(req, res) {
+ var options = {
+   headers: config.headers,
+   url: config.url.gett,
+   body: "consumer_key=" + CONSUMER_KEY + "&access_token=" + req.query.access_token + "&tag=" + req.query.tag
+ };
+
+  request.post(options, function(error, response, body){
+    res.send(JSON.parse(body))
+  })
+ })
 
 /* PUT YOUR CODE ABOVE THIS COMMENT */
 
 var port = process.env.PORT || 3000;
 app.listen(port);
 console.log('Server running on port 3000');
-
-
-/* BreweryDB API Example */
-
-// app.get('/api', function(req, res){ // listens for request on /api route
-//   var lat = req.query.lat; // grabs lat and lng queries from the request object
-//   var lng = req.query.lng;
-//   request('https://api.brewerydb.com/v2/search/geo/point?lat=' + lat + '&lng=' + lng + '&type=beer&hasImages=Y&key=72a751214ab8b53056ac0a6d8376dc2d', function (error, response, body) { // api url
-//     if (!error && response.statusCode === 200) {
-//       console.log('beer');
-//       res.send(body); // if no errors, send the body of data back to front end
-//     }
-//    });
-// });
